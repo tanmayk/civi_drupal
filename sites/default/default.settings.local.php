be8a50aa68bfaa54ffc4c47326eb374b38403590
Tanmay Khedekar <tanmay@osseed.com>
2019-04-16 18:12:34 +0530

    feat: added configuration files for Drupal.

---
+++ sites/default/default.settings.local.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @file
+ * Provides functionality to be used for setting local instance.
+ */
+
+// Disable css and js aggregation.
+$conf['preprocess_css'] = 0;
+$conf['preprocess_js'] = 0;
+
+// Disable caching
+$conf['cache'] = 0;
+
+// Enable full error reporting.
+$conf['error_level'] = 2;
+
+// Configure local database connection.
+$databases = array(
+  'default' =>
+  array(
+    'default' =>
+    array(
+      'database' => '',
+      'username' => '',
+      'password' => '',
+      'host' => '',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
+
+/**
+ * Smart start:
+ *
+ * If you would prefer to be redirected to the installation system when a
+ * valid settings.php file is present but no tables are installed, remove
+ * the leading hash sign below.
+ */
+$conf['pressflow_smart_start'] = TRUE;
