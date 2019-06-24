f3c19778f29f967a14b1658a8128a9fdb3b4ecdb
Dhanesh Dhuri <dhanesh@osseed.com>
2019-06-24 23:00:31 +0530

    Fixed some minor error messages from scheduler [#166537321]

--- profiles/civi_drupal_kit/civicrm_extension/com.osseed.sponsorshipsmails/api/v3/SponsorshipEmails/Sponsorshipemails.php
+++ profiles/civi_drupal_kit/civicrm_extension/com.osseed.sponsorshipsmails/api/v3/SponsorshipEmails/Sponsorshipemails.php
@@ -34,70 +34,78 @@ function civicrm_api3_sponsorship_emails_Sponsorshipemails($params) {
         'is_active' => 1,
       ]);
 
-      $eventListHTML = "<ul>";
-      foreach ($events['values'] as $eventValues) {
-        $URL = CRM_Utils_System::baseURL() . "civicrm/event/register?reset=1&id=" . $eventValues['id'];
-        $eventListHTML .= "<li><a href=" . $URL . ">" . $eventValues['event_title'] . "</a></li>";
-      }
-      $eventListHTML .= "</ul>";
-
-      // Replace event list token with event list.
-      $text_message = str_replace("{event.upcoming_event_list}", $eventListHTML, $text_message);
-      $html_message = str_replace("{event.upcoming_event_list}", $eventListHTML, $html_message);
-
-      // Get from email value.
-      $fromEmailValues = CRM_Core_BAO_Email::getFromEmail();
-      if (is_numeric(key($fromEmailValues))) {
-        $defaultEmail = civicrm_api3('email', 'getsingle', array('id' => key($fromEmailValues)));
-        $fromEmail = $defaultEmail['email'];
-      }
-
-      // Get all contacts emails.
-      $contacts = civicrm_api3('Contact', 'get', [
-        'sequential' => 1,
-        'options' => ['limit' => 0],
-        'contact_type' => "Individual",
-        'do_not_email' => 0,
-        'email' => ['IS NOT NULL' => 1],
-      ]);
-
-      if (!empty($contacts) && $contacts['count'] > 0) {
-        $contactEmail = [];
-        foreach ($contacts['values'] as $key => $value) {
-          if ($fromEmail == $value['email']) {
-            continue;
-          }
-
-          $contactEmail[$value['contact_id']] = $value['email'];
-
-          $toEmail = $value['email'];
-          $toDisplayName = $value['display_name'];
-
-          // Replace contact tokens from message template.
-          foreach ($value as $fieldName => $fieldValue) {
-            $text_message = str_replace("{contact." . $fieldName . "}", $fieldValue, $text_message);
-            $html_message = str_replace("{contact." . $fieldName . "}", $fieldValue, $html_message);
-          }
-
-          // Create the params array.
-          $mailParams = array(
-            'groupName' => 'Sponsorship Email Sender',
-            'from' => $fromEmail,
-            'toName' => $toDisplayName,
-            'toEmail' => $toEmail,
-            'subject' => $subject,
-            'cc' => NULL,
-            'bcc' => NULL,
-            'text' => $text_message,
-            'html' => $html_message,
-            'attachments' => NULL,
-          );
-          // Send emails to each contact.
-          if (!CRM_Utils_Mail::send($mailParams)) {
-            return FALSE;
-          }
+      if (!empty($events['values'])) {
+        $eventListHTML = "<ul>";
+        foreach ($events['values'] as $eventValues) {
+          $URL = CRM_Utils_System::baseURL() . "civicrm/event/register?reset=1&id=" . $eventValues['id'];
+          $eventListHTML .= "<li><a href=" . $URL . ">" . $eventValues['event_title'] . "</a></li>";
         }
-        return civicrm_api3_create_success($contactEmail, $params, NULL, NULL);
+        $eventListHTML .= "</ul>";
+
+        // Replace event list token with event list.
+        $text_message = str_replace("{event.upcoming_event_list}", $eventListHTML, $text_message);
+        $html_message = str_replace("{event.upcoming_event_list}", $eventListHTML, $html_message);
+
+        // Get from email value.
+        $fromEmailValues = CRM_Core_BAO_Email::getFromEmail();
+        if (is_numeric(key($fromEmailValues))) {
+          $defaultEmail = civicrm_api3('email', 'getsingle', array('id' => key($fromEmailValues)));
+          $fromEmail = $defaultEmail['email'];
+        }
+
+        // Get all contacts emails.
+        $contacts = civicrm_api3('Contact', 'get', [
+          'sequential' => 1,
+          'options' => ['limit' => 0],
+          'contact_type' => "Individual",
+          'do_not_email' => 0,
+          'email' => ['IS NOT NULL' => 1],
+        ]);
+
+        if (!empty($contacts) && $contacts['count'] > 0) {
+          $contactEmail = [];
+          foreach ($contacts['values'] as $key => $value) {
+            if ($fromEmail == $value['email']) {
+              continue;
+            }
+
+            $contactEmail[$value['contact_id']] = $value['email'];
+
+            $toEmail = $value['email'];
+            $toDisplayName = $value['display_name'];
+
+            // Replace contact tokens from message template.
+            foreach ($value as $fieldName => $fieldValue) {
+              $text_message = str_replace("{contact." . $fieldName . "}", $fieldValue, $text_message);
+              $html_message = str_replace("{contact." . $fieldName . "}", $fieldValue, $html_message);
+            }
+
+            // Create the params array.
+            $mailParams = array(
+              'groupName' => 'Sponsorship Email Sender',
+              'from' => $fromEmail,
+              'toName' => $toDisplayName,
+              'toEmail' => $toEmail,
+              'subject' => $subject,
+              'cc' => NULL,
+              'bcc' => NULL,
+              'text' => $text_message,
+              'html' => $html_message,
+              'attachments' => NULL,
+            );
+            // Send emails to each contact.
+            if (!CRM_Utils_Mail::send($mailParams)) {
+              return FALSE;
+            }
+          }
+          return civicrm_api3_create_success($contactEmail, $params, NULL, NULL);
+        }
+        else {
+          throw new API_Exception("No any contacts found for sending emails.", 1234);
+        }
+      }
+      else {
+        throw new API_Exception("No upcoming event found in this month.", 1234);
       }
     }
     else {
